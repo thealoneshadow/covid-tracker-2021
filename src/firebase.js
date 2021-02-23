3371add7672122a8d670ea7c4400338885103da8
TheAloneShadow <43110655+thealoneshadow@users.noreply.github.com>
2021-02-23 09:50:41 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSwegsm0ayou19FA0OyaKX1bfaDTleTWU",
+  authDomain: "covid-6e452.firebaseapp.com",
+  projectId: "covid-6e452",
+  storageBucket: "covid-6e452.appspot.com",
+  messagingSenderId: "161363671761",
+  appId: "1:161363671761:web:2c5f2251ec2dbfdcc5e3f0",
+  measurementId: "G-S9BFX8R51D"
+};
